2b3083a38e8d647f34dfec3f227bae8c13b4f864
JuaniFiele <108836078+JuaniFiele@users.noreply.github.com>
2024-07-29 13:54:13 +0000

    Update 1.3.sql

--- 1.3.sql
+++ 1.3.sql
@@ -8,3 +8,14 @@ inner join personas_titulos pert
 	on per.dni=pert.dni
 inner join titulos tit
 	on pert.cod_titulo=tit.cod_titulo
+
+
+/*Resumen
+Agrupación de Datos: Organiza filas en grupos basados en columnas específicas.
+Resúmenes Estadísticos: Calcula estadísticas y resúmenes para cada grupo usando funciones de agregación.
+Eliminación de Duplicados: Proporciona una lista de valores únicos en combinación con columnas.
+Filtrado con HAVING: Aplica condiciones a grupos resultantes después de la agregación.
+Agrupación por Varias Columnas: Realiza resúmenes detallados basados en múltiples columnas.
+Análisis Temporal: Agrupa datos según unidades de tiempo para análisis de tendencias.
+Cada uno de estos usos aprovecha las capacidades de GROUP BY para facilitar el análisis y la organización de datos en SQL.
+*/
